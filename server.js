390bdf03ca756bb455009d31235901c3f2210ded
bornreddy <marisa.reddy@gmail.com>
2014-01-07 18:45:39 -0500

    last name removal

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server = http.createServer(
  
 				   // Create a SUPER SIMPLE response.
    	     response.writeHead( 200, {"content-type": "text/plain"} );
-	     response.write( "Hello world from Marisa Reddy!\n Steps to making this\n repo\n instance\n security on instance\n elastic ip (add in later) \n dns point to elastic ip \n connect script \n ssh into instance \n download node, npm, git, emacs \n clone repo \n redirect port" );
+	     response.write( "Hello world from Marisa!\n Steps to making this\n repo\n instance\n security on instance\n elastic ip (add in later) \n dns point to elastic ip \n connect script \n ssh into instance \n download node, npm, git, emacs \n clone repo \n redirect port" );
 	     response.end();
      }
 			       );
